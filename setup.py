31e2c024beab4d70346b051d12c5d1f73e072df8
Andy Likuski <andy@likuski.org>
2018-11-07 15:54:24 -0800

    Made GeometryCollection into FeatureCollectionDataType to reflect the use of json over the GeosGeometryCollection

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape-graphene",
-    version="0.2.49",
+    version="0.2.50",
     author="Andy Likuski",
     author_email="andy@likuski.org",
     description="Graphene helpers for rescape projects",
