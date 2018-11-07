c62a3cfb633ad6a10d015706ff396963cb2c35e7
Andy Likuski <andy@likuski.org>
2018-11-07 15:41:36 -0800

    Version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape-graphene",
-    version="0.2.47",
+    version="0.2.48",
     author="Andy Likuski",
     author_email="andy@likuski.org",
     description="Graphene helpers for rescape projects",
