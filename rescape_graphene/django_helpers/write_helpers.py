cda70053518fcbd7584b416cc88a693c7f5d3524
Andy Likuski <andy@likuski.org>
2019-01-17 16:22:08 +0100

    Fixed enforce unique props to not require values

--- rescape_graphene/django_helpers/write_helpers.py
+++ rescape_graphene/django_helpers/write_helpers.py
@@ -12,8 +12,8 @@ def increment_prop_until_unique(django_class, strategy, prop, django_instance_da
         the database
     :param django_class: Django class to query
     :param prop: The prop to ensure uniqueness
-    :param strategy: function to try to make a value unique. Expects all potential matching values (all values
-    that begin with the value of the property), the prop value, and the current index. It's called for each matching
+    :param strategy: function to try to make a value unique. Expects all potential matching values--all values
+    that begin with the value of the property--the prop value, and the current index. It's called for each matching
     value to guarentee the strategy will eventually get a unique value. For instance, if prop is key and it equals
     'foo', and 'foo', 'foo1', 'foo2', and 'foo3' are in the db, strategy will be called with an array of 4 values 4
     times, with index 0 through 3. If strategy is None the default strategy is to append index+1 to the duplicate name
@@ -54,15 +54,16 @@ def enforce_unique_props(property_fields, django_instance_data):
     which when present points at a function that expects the django_instance_data and returns the django_instance_data
     modified so that the property in question has a unique value.
     :param django_instance_data: dict of an instance to be created or updated
+    :param for_update if True this is for an update mutation so props are not required
     :return: The modified django_instance_data for any property that needs to have a unique value
     """
 
     # If any prop needs to be unique then run its unique_with function, which updates it to a unique value
     # By querying the database for duplicate. This is mainly for non-pk fields like a key
     return R.reduce(
-        lambda reduced, prop_value: prop_value['unique_with'](reduced) if
-        R.prop_or(False, 'unique_with', prop_value) else
-        reduced,
+        lambda reduced, prop_field_tup: prop_field_tup[1]['unique_with'](reduced) if
+            R.has(prop_field_tup[0], reduced) and R.prop_or(False, 'unique_with', prop_field_tup[1]) else
+            reduced,
         django_instance_data,
-        R.values(property_fields)
+        property_fields.items()
     )
