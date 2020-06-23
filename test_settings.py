db9ebfe7561b6cfecfff6c63a1a87a3778ff017e
Andy Likuski <andy@likuski.org>
2020-06-23 14:14:53 +0200

    Formally listed django_filters in the settings

--- test_settings.py
+++ test_settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'sample_webapp',
+    'django_filters',
     'corsheaders',
     'graphene_django',
     'reversion',
